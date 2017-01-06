cd9b7becd1ebc50fc881206a8ec12c8566af09fb
Tunghsiao Liu <t@sparanoid.com>
2017-01-06 12:15:14 +0800

    feat: init repo

---
+++ comment-author-role-badge.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * Plugin Name: Comment Author Role Badge
+ * Plugin URI: https://sparanoid.com/work/comment-author-role-badge/
+ * Description: Displays a user badge next to the comment author name based on author's role
+ * Author: Tunghsiao Liu
+ * Version: 1.0.0
+ * Author URI: https://sparanoid.com
+ * License: GPL2+
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+if ( ! class_exists( 'Comment_Author_Role_Badge' ) ) :
+
+  class Comment_Author_Role_Badge {
+
+    protected $user_role = '';
+
+    public function __construct() {
+      add_action( 'init', array( $this, 'load_admin_textdomain_in_frontend' ) );
+      add_filter( 'wp_enqueue_scripts', array( $this, 'add_stylesheets' ) );
+      add_filter( 'get_comment_author', array( $this, 'get_comment_author_role' ), 10, 3 );
+      add_filter( 'get_comment_author_link', array( $this, 'comment_author_role' ) );
+    }
+
+    /**
+     * Register text domain
+     *
+     * @since 1.0.0
+     *
+     * @link  https://core.trac.wordpress.org/ticket/37539
+     */
+    public function load_admin_textdomain_in_frontend() {
+      if ( ! is_admin() ) {
+        load_textdomain( 'default', WP_LANG_DIR . '/admin-' . get_locale() . '.mo' );
+      }
+    }
+
+    /**
+     * Modify author link with role badges
+     *
+     * @since  1.0.0
+     */
+    public function get_comment_author_role( $author, $comment_id, $comment ) {
+      global $wp_roles;
+      if ( $wp_roles ) {
+        $reply_user_id = $comment->user_id;
+        if ( $reply_user_id && $reply_user = new WP_User( $reply_user_id ) ) {
+          if ( isset( $reply_user->roles[0] ) ) {
+            $user_role = translate_user_role( $wp_roles->roles[ $reply_user->roles[0] ]['name'] );
+            $this->user_role = '<div class="comment-author-role-badge comment-author-role-badge--'. $reply_user->roles[0] .'">'. $user_role .'</div>';
+          }
+        } else {
+          $this->user_role = '';
+        }
+      }
+      return $author;
+    }
+
+    /**
+     * Update author link ouput
+     *
+     * @since  1.0.0
+     */
+    public function comment_author_role( $author ) {
+      return $author .= $this->user_role;
+    }
+
+    /**
+     * Add stylesheets
+     *
+     * @since  1.0.0
+     */
+    public function add_stylesheets() {
+      wp_enqueue_style( 'comment-author-role-badge', plugins_url( 'comment-author-role-badge.css', __FILE__ ) );
+    }
+  }
+
+  new Comment_Author_Role_Badge;
+
+endif;
---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Comment Author Role Badge ===
+Contributors: Sparanoid
+Donate link: https://sparanoid.com/donate/
+Tags: author, badge, badges, comment, comments, css, theme
+Requires at least: 4.6
+Tested up to: 4.7
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Displays a user badge next to the comment author name based on author's role.
+
+== Description ==
+
+Displays a user badge next to the comment author name based on author's role.
+
+* No database writes, no configurations, install, activate and go.
+* Easy styles customization via Jetpack Custom CSS or built-in Custom CSS (WordPress version 4.7 or higher required).
+* Works with [Current Theme Body Class](https://wordpress.org/plugins/current-theme-body-class/) plugin.
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/` directory, or install the plugin through the WordPress plugins screen directly.
+1. Activate the plugin through the 'Plugins' screen in WordPress
+
+== Frequently Asked Questions ==
+
+= Does this plugin change the content of my comments? =
+
+No. No database read/write for this plugin.
+
+== Screenshots ==
+
+1. Plugin enabled with WordPress official theme Twenty Twelve
+
+== Changelog ==
+
+= 1.0.0 =
+* First release
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+* First release
